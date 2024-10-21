ddee1b751a9fbd2bed30337881f43ec54bb14fb0
MursalinDevOps <mursalin1768@gmail.com>
2024-10-21 15:09:18 +0600

    adding data to local storage and add img to the UI functionality added

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -14,12 +14,22 @@ const Bottles = () => {
             .then(data => setBottles(data))
     }, [])
 
-    useEffect(()=>{
-        if(bottles.length > 0){
+    useEffect(() => {
+        if (bottles.length > 0) {
             const storedCart = getStoredCart();
-            console.log(storedCart)
+            // console.log(storedCart, bottles)
+            const savedCart = [];
+            for (const id of storedCart) {
+                console.log(id)
+                const bottle = bottles.find(bottle => bottle.id === id);
+                if (bottle) {
+                    savedCart.push(bottle)
+                }
+            }
+            // console.log('saved cart', savedCart)
+            setCart(savedCart)
         }
-    },[bottles])
+    }, [bottles])
 
     const handleCart = (bottle) => {
         const newCart = [...cart, bottle];
@@ -30,8 +40,11 @@ const Bottles = () => {
 
     return (
         <div>
-            <h3>My cart : {cart.length}</h3>
             <h3>Total Bottles : {bottles.length}</h3>
+            <h3>My cart : {cart.length}</h3>
+            {
+                cart.map(bottle => <img key={bottle.id} style={{width:'100px', marginRight:'10px'}} src={bottle.img}></img>)
+            }
             <div className="layout">
                 {
                     bottles.map(bottle => <Bottle
